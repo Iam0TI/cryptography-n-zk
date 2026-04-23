23edc22d031bdf7450f534ddde53907dcd4e67c4
iam0ti <80600490+Iam0ti@users.noreply.github.com>
2026-04-23 04:19:32 +0100

    long division

---
+++ long_integer_divison.py
@@ -0,0 +1,60 @@
+# Determine the sign of the quotient, then work with the absolute values of the dividend and divisor.
+# Divide the first digit of the dividend by the divisor. If the divisor is larger than the first digit of the dividend, divide the first two digits of the dividend by the divisor, and so on.
+# Write the quotient above the dividend.
+# Multiply the quotient by the divisor and write the product under the dividend.
+# Subtract that product from the dividend.
+# Bring down the next digit of the dividend.
+# Repeat from Step 1 until there are no more digits in the dividend to bring down.
+# Check by multiplying the quotient times the divisor.
+# Add the sign of division to the quotient of the absolute values.
+# def compute(number,divisor):
+#     if divisor == 0:
+#         raise ValueError("Division by zero is undefined")
+
+#     absn = get_abosolute(number)
+#     absd = get_abosolute(divisor)
+#     quoient = absn // absd
+#     remainder = absn % absd
+  
+#     if (number < 0 or divisor < 0):
+      
+#         quoient = quoient + 1
+#         remainder= divisor - remainder
+#     quoient =  quoient * get_sign(number,divisor)
+#     return divisor,number,remainder,quoient
+
+# def get_abosolute(number):
+#     if number > 0:
+#         return number
+#     else:
+#        return number * -1
+# def get_sign(a,b):
+#     if a < 0 and  b > 0:
+#         return -1
+#     elif a < 0 and b < 0:   
+#         return 1
+#     elif a > 0 and b < 0:
+#         return -1
+#     elif a >0 and b > 0:
+#         return 1
+
+def compute(a, b):
+    if b == 0:
+        raise ValueError("Division by zero is undefined")
+
+    # Python already gives quotient and remainder
+    q = a // b
+    r = a % b
+
+    # to ensure 0 ≤ r < |b| but i don't this code will ever be rreach
+    if r < 0:
+        if b > 0:
+            q -= 1
+            r += b
+        else:
+            q += 1
+            r -= b
+    assert a == q*b+r
+    return q, r
+
+print(f"{compute(-1687,11)}")
